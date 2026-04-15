8c05880363df7d0c3b17fb0e8a4870ada190d079
Raj <mriadhussainraj@gmail.com>
2026-04-16 04:40:44 +0600

    Loan Housing Done

--- lib/Insurance/add_insurance_screen.dart
+++ lib/Insurance/add_insurance_screen.dart
@@ -14,7 +14,7 @@ class AddInsuranceScreen extends StatefulWidget {
   @override
   State<AddInsuranceScreen> createState() => _AddInsuranceScreenState();
 }
-//VVVVVVV
+//VVVVVV
 class _AddInsuranceScreenState extends State<AddInsuranceScreen> {
   final InsuranceService _apiService = InsuranceService();
   final TextEditingController _nameController = TextEditingController();
